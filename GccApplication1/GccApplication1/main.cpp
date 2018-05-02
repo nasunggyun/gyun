7bbc958da3221cfa6eb6b2a5803cf2a4029fdb05
nasunggyun <nasunggyun@gmail.com>
2018-05-02 11:21:56 +0900

    led on/off create

---
+++ GccApplication1/GccApplication1/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * GccApplication1.cpp
+ *
+ * Created: 2018-05-02 오전 11:03:21
+ * Author : stc_165
+ */ 
+
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+	DDRD = 0xff;
+	PORTD = 0xff;
+	/* Replace with your application code */
+	while (1)
+	{
+		PORTD = 0x00;
+		_delay_ms(500);
+		PORTD = 0xff;
+		_delay_ms(500);
+		
+	}
+}
+
